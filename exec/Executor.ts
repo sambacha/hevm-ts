d449ca2549519193a53fd7c29739077b88e9df7a
Sam Bacha <sam@manifoldfinance.com>
2023-08-14 03:10:12 -0700

    feat(repo): refactor jj

--- exec/Executor.ts
+++
@@ -1,6 +0,0 @@
-import { EVM } from '../EVM'
-import { Operation } from '../../../bytecode/Operation'
-
-export interface Executor {
-  execute(op: Operation, evm: EVM)
-}
---
+++ src/cfg/index.ts
@@ -0,0 +1,5 @@
+export * from './CFGBlocks';
+export * from './CFGCreator';
+export * from './EthereumCFGCreator';
+export * from './GraphVizService';
+export * from './OperationBlock';
